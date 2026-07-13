a698bad011a8ec0cfc1c14efb4f5021cc56387ef
Danny Tran <dtran@torontofilmschool.ca>
2026-07-13 19:41:46 -0400

    Week 2 Lab
    Create a character with movement, debug messages & imported/setup assets

--- Source/VGP221Summer2026/Private/FPSProjectGameMode.cpp
+++ Source/VGP221Summer2026/Private/FPSProjectGameMode.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "FPSProjectGameMode.h"
+#include "Logging/StructuredLog.h"
 
 void AFPSProjectGameMode::StartPlay()
 {
@@ -13,4 +13,40 @@ void AFPSProjectGameMode::StartPlay()
 	// Display a debug message for five seconds.
 	// The -1 "Key" value argument prevents the message from being updated or refreshed.
 	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, TEXT("Hello World, this is FPSGameMode! This is with editor open"));
+
+	// 2. Print to log
+	UE_LOG(LogTemp, Warning, TEXT("Hello World"));
+
+	// 3. Printing values using specifiers
+	int TestNumber = 5;
+	bool TestBool = true; //  0 or 1
+	UE_LOG(LogTemp, Warning, TEXT("Test Number: %i"), TestNumber);
+	UE_LOG(LogTemp, Warning, TEXT("Test Bool: %i"), TestBool);
+	UE_LOG(LogTemp, Warning, TEXT("Test Bool String: %s"), TestBool ? TEXT("true") : TEXT("false"));
+
+	// 4. Pointers & References
+	int* TestNumberPtr = &TestNumber;
+
+	UE_LOG(LogTemp, Warning, TEXT("TestNumber Ref: %p"), &TestNumber);
+	UE_LOG(LogTemp, Warning, TEXT("TestNumber Ptr: %p"), TestNumberPtr);
+
+	UE_LOG(LogTemp, Warning, TEXT("This will be large number: %i"), TestNumberPtr);
+
+	UE_LOG(LogTemp, Warning, TEXT("TestNumber: %i"), *TestNumberPtr); // Dereferencing the pointer to get the value it points to
+
+	// 5. Giving a nickname to a variable
+	int& NicknameForTestNumber = TestNumber;
+	NicknameForTestNumber = 10;
+	UE_LOG(LogTemp, Warning, TEXT("New Test Number: %i"), TestNumber);
+
+	// 6. Printing a string
+	FString TestString = TEXT("Hello, Unreal!");
+	UE_LOG(LogTemp, Warning, TEXT("Test String: %s"), *TestString);
+
+	//7. Causing a crash with a pointer
+	int* CrashPointer = nullptr;
+	//UE_LOG(LogTemp, Warning, TEXT("This will cause a crash: %i"), *CrashPointer);
+
+	// 8. Using structured logging
+	UE_LOGFMT(LogTemp, Warning, "Structured Logging: Test Number: {0}, Test Bool: {1}, Test String: {2}", TestNumber, TestBool, *TestString);
 }
